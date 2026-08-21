ff13fb32c9f320ca6f091f811b9090c04825702c
omkarg04 <og628851@gmail.com>
2026-08-21 22:39:12 +0530

    Add tarvis image import to constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,6 +2,7 @@ import project1 from "../assets/projects/project-1.jpg";
 import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import udacityomkar from "../assets/udacityomkar.jpg";
+import tarvis from "../assets/tarvis.jpg";
 
 
 export const HERO_CONTENT = `Hi, I am Omkar 
