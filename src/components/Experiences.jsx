9999d71dd80c7396fe8682eaca331f55e20cef57
omkarg04 <og628851@gmail.com>
2026-09-24 22:11:55 +0530

    Create Experiences.jsx

---
+++ src/components/Experiences.jsx
@@ -0,0 +1,9 @@
+// import { WORK_EXPERIENCES } from "../constants";
+
+// const Experiences = () => {
+
+//   >Experience</motion.h2>
+
+// {WORK_EXPERIENCES.map((experience) => (
+
+//   export default Experiences;
